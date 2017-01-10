dc91c21ee9d0fcc914a7150b225a5412976cb7e3
Ayushman Dash <dash.ayushman.99@gmail.com>
2017-01-10 23:18:55 +0100

    added the anp train module using VGG16

---
+++ train_anp.py
@@ -0,0 +1,280 @@
+import csv
+import os
+import traceback
+import numpy as np
+from keras.applications.vgg16 import VGG16
+from keras.preprocessing import image
+from keras.models import Model
+from keras.layers import Dense, GlobalAveragePooling2D, Flatten, Dropout
+from keras import backend as K
+import skimage
+from skimage.transform import resize
+from skimage import io
+import random
+from keras.models import Sequential, model_from_json, load_model
+import pickle
+
+dataRoot = 'Data/anp'
+datarepo_path = os.path.join(dataRoot, "images")
+target_file_path = os.path.join(dataRoot, "allanps.txt")
+ser_loaded_data_path = os.path.join(dataRoot, "image_dict.pkl")
+tr_image_ids = []
+val_image_ids = []
+image_dict = {}
+target = []
+one_hot_targets = []
+n_targets = 0
+n_instances = 0
+labels = []
+
+def one_hot_encode_str_lbl(lbl, target, one_hot_targets):
+    '''
+    Encodes a string label into one-hot encoding
+
+    Example:
+        input: "window"
+        output: [0 0 0 0 0 0 1 0 0 0 0 0]
+    the length would depend on the number of classes in the dataset. The
+    above is just a random example.
+
+    :param lbl: The string label
+    :return: one-hot encoding
+    '''
+    idx = target.index(lbl)
+    return one_hot_targets[idx]
+
+
+def get_one_hot_targets(target_file_path):
+    target = []
+    one_hot_targets = []
+    n_target = 0
+    try:
+        with open(target_file_path) as f:
+            target = f.readlines()
+            target = [t.strip('\n') for t in target]
+            n_target = len(target)
+    except IOError:
+        print('Could not load the labels.txt file in the dataset. A '
+              'dataset folder is expected in the "data/datasets" '
+              'directory with the name that has been passed as an '
+              'argument to this method. This directory should contain a '
+              'file called labels.txt which contains a list of labels and '
+              'corresponding folders for the labels with the same name as '
+              'the labels.')
+        traceback.print_stack()
+
+    lbl_idxs = np.arange(n_target)
+    one_hot_targets = np.zeros((n_target, n_target))
+    one_hot_targets[np.arange(n_target), lbl_idxs] = 1
+
+    return target, one_hot_targets, n_target
+
+def load_data(data_dir, target_file_path, ser_file_path):
+    data_path = os.path.join(data_dir, 'english.csv')
+    labels = []
+    image_ids = []
+    image_dict = {}
+    n_instances = 0
+    if os.path.exists(os.path.join(dataRoot, 'image_dict.pkl')) and \
+        os.path.exists(os.path.join(dataRoot, 'labels.pkl')) and \
+        os.path.exists(os.path.join(dataRoot, 'image_ids.pkl')):
+        print('Loading saved data objects. It might take some time so sit '
+              'back and relax.')
+        image_dict = pickle.load(open(os.path.join(dataRoot, 'image_dict.pkl')))
+        labels = pickle.load(open(os.path.join(dataRoot, 'labels.pkl')))
+        image_ids = pickle.load(open(os.path.join(dataRoot, 'image_ids.pkl')))
+        n_instances = len(image_dict.keys())
+    else:
+        with open(data_path, 'rb') as csvfile:
+            rows = csv.DictReader(csvfile, delimiter=',', )
+            for i, row in enumerate(rows):
+                if i % 100 == 0 and i != 0:
+                    print('Extracting Row ' + str(i))
+                labels.append(row['ANP'])
+                image_ids.append(i)
+                image_dict[i] = {
+                    'classlbl': row['ANP'],
+                    'image_url': row['image_url']
+                }
+
+                n_instances += 1
+        pickle.dump(image_dict, open(os.path.join(dataRoot, 'image_dict.pkl'),
+                                     "wb"))
+        pickle.dump(labels, open(os.path.join(dataRoot, 'labels.pkl'),
+                                     "wb"))
+        pickle.dump(image_ids, open(os.path.join(dataRoot, 'image_ids.pkl'),
+                                     "wb"))
+    target = None
+    n_targets = None
+    one_hot_targets = None
+
+    if not os.path.exists(target_file_path):
+        with open(target_file_path, "w") as text_file:
+            targets = set(labels)
+            text_file.write('\n'.join(targets))
+        target, one_hot_targets, n_target = get_one_hot_targets(
+            target_file_path)
+    else:
+        target, one_hot_targets, n_target = get_one_hot_targets(
+            target_file_path)
+
+    return image_ids, image_dict, target, one_hot_targets, n_targets, \
+                n_instances
+
+def load_process_images(image_ids, new_shape=(244, 244, 3)):
+    processed_images = []
+    img = None
+    for image_id in image_ids:
+        if os.path.exists(os.path.join(datarepo_path, str(image_id) + '.png')):
+            img = io.imread(os.path.join(datarepo_path, str(image_id) + '.png'))
+        else:
+            img = io.imread(image_dict[image_id]['image_url'])
+            io.imsave(os.path.join(datarepo_path, str(image_id) + '.png'), img)
+        img = resize(img, new_shape)
+        processed_images.append(img.tolist())
+    return processed_images
+
+def process_lbls(image_ids):
+    one_hot_encoded_lbls = []
+    for image_id in image_ids:
+        one_hot_encoded_lbls.append(
+                    one_hot_encode_str_lbl(image_dict[image_id]['classlbl'],
+                                           target, one_hot_targets).tolist())
+    return one_hot_encoded_lbls
+
+def get_batch(start, batch_size=64, dataType='train', new_shape=(224, 224, 3)):
+    end = start + batch_size
+    chk_len = len(tr_image_ids) if dataType == 'train' else len(val_image_ids)
+    if end >= chk_len:
+        return None, None
+    batch_image_ids = None
+    if dataType == 'train':
+        batch_image_ids = tr_image_ids[start : end]
+    else:
+        batch_image_ids = val_image_ids[start: end]
+    image_batches = load_process_images(batch_image_ids, new_shape)
+    one_hot_encoded_lbls = process_lbls(batch_image_ids)
+    return np.array(image_batches), np.array(one_hot_encoded_lbls)
+
+
+def build_model(n_classes):
+    base_model = VGG16(weights='imagenet', include_top=False, input_shape=(
+        224,224,3))
+
+    # add a global spatial average pooling layer
+    x = base_model.output
+    x = Flatten()(x)
+    # let's add a fully-connected layer
+    x = Dense(6024, activation='relu')(x)
+    x = Dropout(0.5)(x)
+    x = Dense(5402, activation='relu')(x)
+    # and a logistic layer -- let's say we have 200 classes
+    predictions = Dense(4421, activation='softmax')(x)
+    # let's add a fully-connected layer
+
+    # and a logistic layer -- let's say we have 200 classes
+
+
+    # this is the model we will train
+    model = Model(input=base_model.input, output=predictions)
+    # first: train only the top layers (which were randomly initialized)
+    # i.e. freeze all convolutional InceptionV3 layers
+    for layer in base_model.layers:
+        layer.trainable = False
+
+    # compile the model (should be done *after* setting layers to non-trainable)
+    model.compile(optimizer='adam', loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    return model
+
+
+if os.path.exists(os.path.join(dataRoot, 'train_ids.pkl')) and \
+        os.path.exists(os.path.join(dataRoot, 'val_ids.pkl')):
+    tr_image_ids = pickle.load(open(os.path.join(dataRoot, 'train_ids.pkl')))
+    val_image_ids = pickle.load(open(os.path.join(dataRoot, 'val_ids.pkl')))
+else:
+    image_ids, image_dict, target, one_hot_targets, n_targets, \
+    n_instances = load_data(dataRoot, target_file_path, ser_loaded_data_path)
+
+    random.shuffle(image_ids)
+    n_train_instances = int(n_instances * 0.75)
+
+    tr_image_ids = image_ids[0 :n_train_instances]
+    val_image_ids = image_ids[n_train_instances : -1]
+
+
+
+def train():
+    model = None
+    batch_size=128
+    if os.path.exists(os.path.join(dataRoot, "model.json")):
+        with open(os.path.join(dataRoot, "model.json"), 'r') as f:
+            json_string = f.read()
+            model = model_from_json(json_string)
+            model.compile(optimizer='adam', loss='categorical_crossentropy',
+                          metrics=['accuracy'])
+    else:
+        model = build_model(n_targets)
+        model_json_string = model.to_json()
+        with open(os.path.join(dataRoot, "model.json"), "w") as f:
+            f.write(model_json_string)
+
+    if os.path.exists(os.path.join(dataRoot, "model.h5")):
+        print('Loading model from previously saved weights')
+        model = load_model(os.path.join(dataRoot, "model.h5"))
+
+    for i in range(30):
+        batch_count = 0
+        while(1):
+            start = batch_count*batch_size
+            X_train, y_train = None, None
+            try:
+                X_train, y_train = get_batch(start, dataType='train',
+                                             batch_size=128)
+            except Exception as e:
+                print('Error occurred while Getting Batch. Skipping this '
+                      'batch.')
+                traceback.print_stack()
+                batch_count += 1
+                continue
+            if X_train is None or y_train is None:
+                break
+            [loss, accuracy] = model.train_on_batch(X_train, y_train)
+            batch_count += 1
+            if batch_count % 1 == 0 and batch_count != 0:
+                print('Itr ' + str(batch_count) + '\ttraining loss: ' + str(loss) +
+                '\ttraining accuracy: ' + str(accuracy))
+            if batch_count % 100 == 0 and batch_count != 0:
+                model.save(os.path.join(dataRoot, 'model.h5'))
+                print('Saving trained model')
+                try:
+                    X_val, y_val = get_batch(0, dataType='val', batch_size=100)
+                    [loss, accuracy] = model.test_on_batch(X_val, y_val)
+                    print('testing loss: ' + str(loss))
+                    print('testing accuracy: ' + str(accuracy))
+                except Exception as e:
+                    print('Error occurred while Getting Batch. Skipping this '
+                          'batch.')
+                    traceback.print_stack()
+                    continue
+                # save as JSON
+
+
+        if i % 2 == 0 and i != 0:
+            model.save(os.path.join(dataRoot, 'model.h5'))
+            # save as JSON
+            print('Saving trained model')
+            try:
+                X_val, y_val = get_batch(0, dataType='val', batch_size=100)
+                [loss, accuracy] = model.test_on_batch(X_val, y_val)
+                print('testing loss: ' + str(loss))
+                print('testing accuracy: ' + str(accuracy))
+            except Exception as e:
+                print('Error occurred while Getting Batch. Skipping this '
+                      'batch.')
+                traceback.print_stack()
+                continue
+
+if __name__ == '__main__':
+    train()
--- train_encoder.py
+++ train_encoder.py
@@ -4,6 +4,8 @@ Gets to 0.8498 test accuracy after 2 epochs. 41s/epoch on K520 GPU.
 '''
 from __future__ import print_function
 import numpy as np
+import sys
+
 np.random.seed(1337)  # for reproducibility
 
 from keras.preprocessing import sequence
@@ -16,6 +18,8 @@ from keras.datasets import imdb
 import pickle, random
 import os
 from pycocotools.coco import COCO
+import sklearn
+from sklearn.metrics import jaccard_similarity_score
 
 # Embedding
 max_features = 25000
@@ -34,25 +38,24 @@ lstm_output_size = 128
 batch_size = 30
 nb_epoch = 2
 
-
 '''
 Note:
 batch_size is highly sensitive.
 Only 2 epochs are needed as the dataset is very small.
 '''
 
-dataRoot='Data'
-dataDir='Data/mscoco'
-tr_dataType='train2014'
-tr_annFile='%s/annotations_inst/instances_%s.json'%(dataDir,tr_dataType)
+dataRoot = 'Data'
+dataDir = 'Data/mscoco'
+tr_dataType = 'train2014'
+tr_annFile = '%s/annotations_inst/instances_%s.json' % (dataDir, tr_dataType)
 
 tr_image_list = []
 tr_n_imgs = 0
 tr_coco_captions = {}
 tr_img_classes = {}
 
-val_dataType='val2014'
-val_annFile='%s/annotations_inst/instances_%s.json'%(dataDir,val_dataType)
+val_dataType = 'val2014'
+val_annFile = '%s/annotations_inst/instances_%s.json' % (dataDir, val_dataType)
 
 val_image_list = []
 val_n_imgs = 0
@@ -60,125 +63,144 @@ val_coco_captions = {}
 val_img_classes = {}
 
 
-def load_data() :
-	tr_coco_captions = pickle.load(open(os.path.join(dataDir, 'train', 'coco_tv.pkl'), "rb"))
-	# h1 = h5py.File(join(data_dir, 'flower_tc.hdf5'))
-	tr_img_classes = pickle.load(open(os.path.join(dataDir, 'train', 'coco_tc.pkl'), "rb"))
-	
-	val_coco_captions = pickle.load(open(os.path.join(dataDir, 'val', 'coco_tv.pkl'), "rb"))
-	# h1 = h5py.File(join(data_dir, 'flower_tc.hdf5'))
-	val_img_classes = pickle.load(open(os.path.join(dataDir, 'val', 'coco_tc.pkl'), "rb"))
-	
-	n_classes = 80
-	max_caps_len = 25
-	
-	tr_coco = COCO(tr_annFile)
-	val_coco = COCO(val_annFile)
-	tr_image_list = tr_coco.getImgIds()
-	tr_n_imgs = len(tr_image_list)
+def load_data():
+    tr_coco_captions = pickle.load(
+        open(os.path.join(dataDir, 'train', 'coco_tv.pkl'), "rb"))
+    # h1 = h5py.File(join(data_dir, 'flower_tc.hdf5'))
+    tr_img_classes = pickle.load(
+        open(os.path.join(dataDir, 'train', 'coco_tc.pkl'), "rb"))
 
-	val_image_list = val_coco.getImgIds()
-	val_n_imgs = len(val_image_list)
-	
-	return tr_coco_captions, tr_img_classes, val_coco_captions, val_img_classes, \
-                    n_classes, max_caps_len, tr_image_list, tr_n_imgs, val_image_list, \
-                    val_n_imgs
-	
-def get_batch(dataType='train', batch_size = 64):
-	X = []
-	y = np.zeros((batch_size, 80))
-	if dataType == 'train':
-		random.shuffle(tr_image_list)
-		batch_idx = np.random.randint(0, tr_n_imgs, size=batch_size)
-		imgs = np.take(tr_image_list, batch_idx)
-		for i, img in enumerate(imgs):
-			X.append(tr_coco_captions[img][np.random.randint(0,4)])
-			#print('Maja')
-			#print(tr_img_classes[img])
-			#print(tr_img_classes[img].shape)
-			y[i: ] = tr_img_classes[img]
-		return np.array(X), y
-	if dataType == 'val':
-		random.shuffle(val_image_list)
-		batch_idx = np.random.randint(0, val_n_imgs, size=batch_size)
-		imgs = np.take(val_image_list, batch_idx)
-		for i, img in enumerate(imgs) :
-			X.append(val_coco_captions[img][np.random.randint(0,4)])
-			y[i :] = val_img_classes[img]
-		return np.array(X), y
+    val_coco_captions = pickle.load(
+        open(os.path.join(dataDir, 'val', 'coco_tv.pkl'), "rb"))
+    # h1 = h5py.File(join(data_dir, 'flower_tc.hdf5'))
+    val_img_classes = pickle.load(
+        open(os.path.join(dataDir, 'val', 'coco_tc.pkl'), "rb"))
+
+    n_classes = 80
+    max_caps_len = 25
+
+    tr_coco = COCO(tr_annFile)
+    val_coco = COCO(val_annFile)
+    tr_image_list = tr_coco.getImgIds()
+    tr_n_imgs = len(tr_image_list)
+
+    val_image_list = val_coco.getImgIds()
+    val_n_imgs = len(val_image_list)
+
+    return tr_coco_captions, tr_img_classes, val_coco_captions, \
+           val_img_classes, \
+           n_classes, max_caps_len, tr_image_list, tr_n_imgs, val_image_list, \
+           val_n_imgs
+
+
+def get_batch(dataType='train', batch_size=64):
+    X = []
+    y = np.zeros((batch_size, 80))
+    if dataType == 'train':
+        random.shuffle(tr_image_list)
+        batch_idx = np.random.randint(0, tr_n_imgs, size=batch_size)
+        imgs = np.take(tr_image_list, batch_idx)
+        for i, img in enumerate(imgs):
+            X.append(tr_coco_captions[img][np.random.randint(0, 4)])
+            # print('Maja')
+            # print(tr_img_classes[img])
+            # print(tr_img_classes[img].shape)
+            y[i:] = tr_img_classes[img]
+        return np.array(X), y
+    if dataType == 'val':
+        random.shuffle(val_image_list)
+        batch_idx = np.random.randint(0, val_n_imgs, size=batch_size)
+        imgs = np.take(val_image_list, batch_idx)
+        for i, img in enumerate(imgs):
+            X.append(val_coco_captions[img][np.random.randint(0, 4)])
+            y[i:] = val_img_classes[img]
+        return np.array(X), y
+
 
 def build_model():
-	print('Build model...')
-	
-	model = Sequential()
-	model.add(Embedding(max_features, embedding_size, input_length=maxlen))
-	model.add(Dropout(0.25))
-	model.add(Convolution1D(nb_filter=nb_filter,
-	                        filter_length=filter_length,
-	                        border_mode='valid',
-	                        activation='relu',
-	                        subsample_length=1))
-	model.add(MaxPooling1D(pool_length=pool_length))
-	model.add(BatchNormalization())
-	model.add(Convolution1D(nb_filter=128,
-	                        filter_length=5,
-	                        border_mode='same',
-	                        activation='relu',
-	                        subsample_length=1))
-	# model.add(MaxPooling1D(pool_length=2))
-	model.add(BatchNormalization())
-	model.add(Convolution1D(nb_filter=256,
-	                        filter_length=3,
-	                        border_mode='valid',
-	                        activation='relu',
-	                        subsample_length=1))
-	model.add(MaxPooling1D(pool_length=2))
-	model.add(BatchNormalization())
-	model.add(LSTM(lstm_output_size))
-	model.add(Dense(80))
-	model.add(Activation('sigmoid'))
-	
-	model.compile(loss='categorical_crossentropy',
-	              optimizer='adam',
-	              metrics=['accuracy'])
-	return model
+    print('Build model...')
 
-def test():
-	pass
+    model = Sequential()
+    model.add(Embedding(max_features, embedding_size, input_length=maxlen))
+    model.add(Dropout(0.25))
+    model.add(Convolution1D(nb_filter=nb_filter,
+                            filter_length=filter_length,
+                            border_mode='valid',
+                            activation='relu',
+                            subsample_length=1))
+    model.add(MaxPooling1D(pool_length=pool_length))
+    model.add(BatchNormalization())
+    model.add(Convolution1D(nb_filter=128,
+                            filter_length=5,
+                            border_mode='same',
+                            activation='relu',
+                            subsample_length=1))
+    # model.add(MaxPooling1D(pool_length=2))
+    model.add(BatchNormalization())
+    model.add(Convolution1D(nb_filter=256,
+                            filter_length=3,
+                            border_mode='valid',
+                            activation='relu',
+                            subsample_length=1))
+    model.add(MaxPooling1D(pool_length=2))
+    model.add(BatchNormalization())
+    model.add(LSTM(lstm_output_size))
+    model.add(Dense(80))
+    model.add(Activation('sigmoid'))
+
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+    return model
+
+
+def test(model, k=1):
+    X_val, y_val = get_batch(dataType='val', batch_size=40000)
+    y_pred = model.predict(X_val, batch_size=64, verbose=1)
+    sim_sum = 0.0
+    for i, (y_dash, y) in enumerate(zip(y_val, y_pred)):
+        top_k = y.argsort()[-k:][::-1]
+        y[top_k] = 1
+        low_values_indices = y < 1
+        y[low_values_indices] = 0
+        sim_sum += jaccard_similarity_score(y_dash, y)
+    score = sim_sum / 40000
+    print('score for K(' + str(k) + ') : ' + str(score))
+
 
 def train():
-	model = None
-	
-	if os.path.exists(os.path.join(dataDir, "model.json")):
-		with open(os.path.join(dataDir, "model.json"), 'r') as f :
-			json_string = f.read()
-			model = model_from_json(json_string)
-	else:
-		model = build_model()
-		model_json_string = model.to_json()
-		with open(os.path.join(dataDir, "model.json"), "w") as f :
-			f.write(model_json_string)
-		
-	if os.path.exists(os.path.join(dataDir, "model.h5")):
-		model = load_model(os.path.join(dataDir, "model.h5"))
-	
-	for i in range(100000):
-		
-		X_train, y_train = get_batch(dataType='train', batch_size=128)
-		[loss, accuracy] = model.train_on_batch(X_train, y_train)
-		
-		if i % 100 == 0 and i != 0 :
-			print('Training iteration ' + str(i))
-			print('training loss: ' + str(loss))
-			print('training accuracy: ' + str(accuracy))
-		if i%100 == 0 and i != 0:
-			X_val, y_val = get_batch(dataType='val', batch_size=1000)
-			[loss, accuracy] = model.test_on_batch(X_val, y_val)
-			print('testing loss: ' + str(loss))
-			print('testing accuracy: ' + str(accuracy))
-			model.save(os.path.join(dataDir, 'model.h5'))
-			# save as JSON
-			print('Saving trained model')
+    model = None
+
+    if os.path.exists(os.path.join(dataDir, "model.json")):
+        with open(os.path.join(dataDir, "model.json"), 'r') as f:
+            json_string = f.read()
+            model = model_from_json(json_string)
+    else:
+        model = build_model()
+        model_json_string = model.to_json()
+        with open(os.path.join(dataDir, "model.json"), "w") as f:
+            f.write(model_json_string)
+
+    if os.path.exists(os.path.join(dataDir, "model.h5")):
+        model = load_model(os.path.join(dataDir, "model.h5"))
+
+    for i in range(100000):
+
+        X_train, y_train = get_batch(dataType='train', batch_size=128)
+        [loss, accuracy] = model.train_on_batch(X_train, y_train)
+
+        if i % 100 == 0 and i != 0:
+            print('Training iteration ' + str(i))
+            print('training loss: ' + str(loss))
+            print('training accuracy: ' + str(accuracy))
+        if i % 100 == 0 and i != 0:
+            X_val, y_val = get_batch(dataType='val', batch_size=1000)
+            [loss, accuracy] = model.test_on_batch(X_val, y_val)
+            print('testing loss: ' + str(loss))
+            print('testing accuracy: ' + str(accuracy))
+            model.save(os.path.join(dataDir, 'model.h5'))
+            # save as JSON
+            print('Saving trained model')
 
 
 print('Loading data...')
@@ -187,7 +209,20 @@ n_classes, max_caps_len, tr_image_list, tr_n_imgs, val_image_list, \
 val_n_imgs = load_data()
 
 if __name__ == '__main__':
-    train()
-    test()
+    # train()
+    model = None
+    if os.path.exists(os.path.join(dataDir, "model.json")):
+        with open(os.path.join(dataDir, "model.json"), 'r') as f:
+            json_string = f.read()
+            model = model_from_json(json_string)
+    else:
+        print('Fucked up')
+        sys.exit(0)
 
-	
+    if os.path.exists(os.path.join(dataDir, "model.h5")):
+        model = load_model(os.path.join(dataDir, "model.h5"))
+    else:
+        print('Fucked up')
+        sys.exit(0)
+    for i in range(10):
+        test(model, k=i)
